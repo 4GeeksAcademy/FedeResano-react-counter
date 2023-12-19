f0fa073cab45ff9e43688081a7e1cf1d9bd91c37
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-12-19 00:56:17 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -3,22 +3,22 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import "../styles/index.css";
 
-function simpleCounter(props) {
+const SimpleCounter = (props) => {
     return (
         <div className="counterBox">
             <div className="clock">
                 <i className="fa-regular fa-clock"></i>
             </div>
-            <div className="sixthDigit">{props.sixth % 10}</div>
-            <div className="fifthDigit">{props.fifth % 10}</div>
-            <div className="fourthDigit">{props.fourth % 10}</div>
-            <div className="thirdDigit">{props.third % 10}</div>
-            <div className="secondDigit">{props.second % 10}</div>
-            <div className="firstDigit">{props.first % 10}</div>
+            <div className="sixthDigit">{props.sixth}</div>
+            <div className="fifthDigit">{props.fifth}</div>
+            <div className="fourthDigit">{props.fourth}</div>
+            <div className="thirdDigit">{props.third}</div>
+            <div className="secondDigit">{props.second}</div>
+            <div className="firstDigit">{props.first}</div>
         </div>);
 }
 
-simpleCounter.propTypes = {
+SimpleCounter.propTypes = {
     sixthDigit: PropTypes.number,
     fifthDigit: PropTypes.number,
     fourthDigit: PropTypes.number,
@@ -30,16 +30,16 @@ simpleCounter.propTypes = {
 let counter = 0;
 
 setInterval(function () {
-    const sixth = Math.floor(counter / 100000);
-    const fifth = Math.floor(counter / 10000);
-    const fourth = Math.floor(counter / 1000);
-    const third = Math.floor(counter / 100);
-    const second = Math.floor(counter / 10);
-    const first = Math.floor(counter / 1);
+    let sixth = Math.floor(counter / 100000) % 10;
+    let fifth = Math.floor(counter / 10000) % 10;
+    let fourth = Math.floor(counter / 1000) % 10;
+    let third = Math.floor(counter / 100) % 10;
+    let second = Math.floor(counter / 10) % 10;
+    let first = Math.floor(counter / 1) % 10;
     counter++;
 
     ReactDOM.render(
-        <simpleCounter
+        <SimpleCounter
             firstDigit={first}
             secondDigit={second}
             thirdDigit={third}
